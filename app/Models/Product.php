f39bfb595368a28403bb828ec3c07abf35aa4e24
Nestor Parra <nestor.parra@cotel.com.co>
2021-01-19 16:05:15 -0500

    Modelo producto validación metodo disponibilidad

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no_disponible';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isDisponible(){
+        return $this->status === self::PRODUCTO_DISPONIBLE;
+    }
 }
