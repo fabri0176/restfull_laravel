7c9937377cf879b9b8df6ecb458f537ce46184c8
Nestor Parra <nestor.parra@cotel.com.co>
2021-01-20 16:03:01 -0500

    Configuración User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'verified' => $verificado = $this->faker->randomElement([User::USER_VERIFICADO, User::USER_NO_VERIFICADO]),
+            'verification_token' => $verificado === User::USER_VERIFICADO ? null : User::generarVerificationToken(),
+            'admin' => $this->faker->randomElement([User::USER_REGULAR, User::USER_ADMIN]),
         ];
     }
 }
