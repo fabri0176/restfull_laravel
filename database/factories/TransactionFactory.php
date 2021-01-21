41d4406501e4a705d351b280b01987ed9d0485c3
Nestor Parra <nestor.parra@cotel.com.co>
2021-01-21 17:44:37 -0500

    Definicion factory transacciones

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Buyer;
+use App\Models\Product;
+use App\Models\Seller;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Transaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        //Se obtienen todos los usuarios que ya tienen un producto
+        $vendedor = Seller::has('products')->get()->random();
+
+        //usuarios que no es igual al id de usuario vendedor
+        $comprador = User::all()->except($vendedor->id)->random();
+
+
+        return [
+            'name' => $this->faker->word(),
+            'quantity' => $this->faker->numberBetween(1, 3),
+            'buyer_id' => $comprador->id,
+            'product_id' => $vendedor->products->random(),
+
+        ];
+    }
+}
