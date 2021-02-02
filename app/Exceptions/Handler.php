1e844857aa42e390f69d5061eca5c4efbe92972c
Nestor Parra <nestor.parra@cotel.com.co>
2021-02-02 16:29:13 -0500

    Exepción cuando se elimina un registro con datos relacionados

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -85,6 +86,14 @@ class Handler extends ExceptionHandler
 
         if ($e instanceof HttpException) {
             return $this->errorResponse($e->getMessage(), $e->getStatusCode());
+        }
+
+        if ($e instanceof QueryException) {
+            $codigo = $e->errorInfo[1];
+            if($codigo == 1451){
+                return $this->errorResponse("No es posible eliminar un registro relacionado de forma permanente", 409);
+            }
+
         }
 
         return parent::render($request, $e);
